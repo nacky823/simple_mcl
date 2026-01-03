537da006ed58af1a068d08222290ee9465ffdcbf
nacky823 <youjiyongmu4@gmail.com>
2026-01-03 19:55:47 +0900

    Update main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include "simple_mcl/particle.hpp"
 #include "simple_mcl/random.hpp"
 #include "simple_mcl/types.hpp"
 
@@ -20,6 +21,22 @@ int main() {
     double mean = sum / n;
     double var = sum_sq / n - mean * mean;
     std::cout << "normal(0,1) sample mean=" << mean << " var=" << var << "\n";
+
+    const double kPi = 3.141592653589793;
+    simple_mcl::Pose min_pose{0.0, 0.0, -kPi};
+    simple_mcl::Pose max_pose{10.0, 10.0, kPi};
+    std::vector<simple_mcl::Particle> particles =
+        simple_mcl::initializeUniformParticles(5, min_pose, max_pose, rng);
+
+    double wsum = 0.0;
+    for (const auto &p : particles) {
+        wsum += p.weight;
+    }
+    std::cout << "particles=" << particles.size() << " weight_sum=" << wsum << "\n";
+    if (!particles.empty()) {
+        const auto &p0 = particles.front();
+        std::cout << "p0=(" << p0.pose.x << ", " << p0.pose.y << ", " << p0.pose.theta << ")\n";
+    }
 
     return 0;
 }
