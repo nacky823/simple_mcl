2ac834726cdd2ea045d5af9690846abe4f5bb81e
nacky823 <youjiyongmu4@gmail.com>
2026-01-05 20:47:20 +0900

    Update constructor

--- include/simple_mcl/visualization.hpp
+++ include/simple_mcl/visualization.hpp
@@ -12,7 +12,16 @@ namespace simple_mcl {
 class Visualizer
 {
 public:
-    Visualizer()
+    Visualizer(
+        double min_x, double max_x, double min_y, double max_y,
+        int render_scale = 10, int max_width = 800, int max_height = 800)
+        : min_x_(min_x),
+          max_x_(max_x),
+          min_y_(min_y),
+          max_y_(max_y),
+          render_scale_(render_scale),
+          max_width_(max_width),
+          max_height_(max_height)
     {
         int width = static_cast<int>(std::ceil((max_x_ - min_x_) * render_scale_));
         int height = static_cast<int>(std::ceil((max_y_ - min_y_) * render_scale_));
