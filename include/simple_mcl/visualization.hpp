f92b85e7abf57fe72608bb3baf297d814773d676
nacky823 <youjiyongmu4@gmail.com>
2026-01-05 20:22:26 +0900

    Add visualization.hpp

---
+++ include/simple_mcl/visualization.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <cmath>
+
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+#include "simple_mcl/particle.hpp"
+
+namespace simple_mcl {
+
+class Visualizer
+{
+public:
+
+private:
+
+};
+
+}  // namespace simple_mcl
